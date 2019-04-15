b03f6d4140081ea047d528ee696dfac1010e7a0b
tosh7 <zlia.6.lj.425@gmail.com>
2019-04-15 15:45:48 +0900

    set up init

---
+++ RealmSampleApp/Sources/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  RealmSampleApp
+//
+//  Created by Satoshi Komatsu on 2019/04/15.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ RealmSampleApp/Sources/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  RealmSampleApp
+//
+//  Created by Satoshi Komatsu on 2019/04/15.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
