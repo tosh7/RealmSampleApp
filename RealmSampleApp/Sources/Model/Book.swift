8b9a953bfddd3f1e90e2ab963f867b1f1aea97c4
tosh7 <zlia.6.lj.425@gmail.com>
2019-04-18 00:40:37 +0900

    Added RealmModel

---
+++ RealmSampleApp/Sources/Model/Book.swift
@@ -0,0 +1,21 @@
+//
+//  Book.swift
+//  RealmSampleApp
+//
+//  Created by Satoshi Komatsu on 2019/04/18.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+final class Book: Object {
+    
+    @objc dynamic var bookTitle: String = ""
+    @objc dynamic var authorName: String = ""
+    @objc dynamic var publishDate: Date = Date()
+    @objc dynamic var publisher: String = ""
+    @objc dynamic var bookImage: Data = Data()
+    
+    
+}
