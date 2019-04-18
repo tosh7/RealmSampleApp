234b22495811ee6bd676126f4d11b46776eea157
tosh7 <zlia.6.lj.425@gmail.com>
2019-04-18 15:33:56 +0900

    とりあえずcommit

--- RealmSampleApp/Sources/ViewController/RegisterViewController.swift
+++ RealmSampleApp/Sources/ViewController/RegisterViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import RealmSwift
 
 final class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    @IBOutlet weak var publishDateTextField: UITextField!
+    @IBOutlet weak var publisherTextField: UITextField!
+    @IBOutlet weak var bookImageButton: UIButton!
+    
+    let realm = try! Realm()
+    var book = Book()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        
+    }
+    
+    @IBAction func setBookImage(_ sender: Any) {
+    }
+    
+    //textField以外の部分をタップするとキーボードをしまう
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
+
+extension RegisterViewController: UITextFieldDelegate {
+    
+    //エンターを押すとキーボードをしまう
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
+
+extension RegisterViewController: UIImagePickerControllerDelegate,  UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        <#code#>
+    }
+    
+    func useCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let picker = UIImagePickerController()
+            
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.allowsEditing = true
+        }
     }
 }
